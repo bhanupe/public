60cd6e4c3741751df4e79dd32711d9641a9afd02
Vijay Peddimsetti <vpeddimsetti@apple.com>
2025-03-02 15:12:34 -0600

    PG AIML - ML - Employee Turnover

--- analytical_processors/employee_turnover.py
+++ analytical_processors/employee_turnover.py
@@ -52,30 +52,69 @@ if __name__ == "__main__":
         # Apply department mapping
         data['department_encoded'] = data["department"].map(department_mapping)
         explain(data)
-        data2 = pd.get_dummies(data, dtype=int)
-        data2['left_str'] = 'no'
-        data2.loc[data2['left'] == 1, 'left_str'] = 'yes'
-        data3 = pd.get_dummies(data2, dtype=int)
-        data3.drop(columns=['left', 'salary_encoded', 'department_encoded'], inplace=True)
-        data3.to_csv('out.csv', index=False)
-        print(data3.head(3))
-        target_cols = data3.columns[-2:]
-        print(target_cols)
-        predictors = data3.drop(columns=target_cols)  # X
-        target = data3[target_cols[1]]  # y
-        print(predictors.shape)
-        print(target.shape)
-        # print(target)
 
-        # X_train, X_test, y_train, y_test = split(predictors, target, test_size=0.30, random_state=12)
-        # print(X_test.head(3))
-        # print(y_test.head(3))
-        # logreg = LogisticRegression(max_iter=5000)
-        # logreg.fit(X_train, y_train)
-        # pred_prob = logreg.predict_proba(X_test)
-        # pred_class = logreg.predict(X_test)
-        # for i in range(0, 3, 1):
-        #     print(pred_prob[i], pred_class[i])
+        # Remove all object type data values
+        data.drop(columns=['department', 'salary'], inplace=True)
+        explain(data)
+
+        corr_mat = data.corr()
+
+        mask = np.ones_like(corr_mat)
+        mask[np.tril_indices_from(mask)] = 0
+
+        # in the correlation matrix, value
+        # The correlation values range from -1 to 1:
+            # 1 means perfect positive correlation,
+            # -1 means perfect negative correlation,
+            # 0 means no correlation.
+            # Based on a predefined threshold (for example, 0.9), you can identify pairs of columns that are highly correlated and drop them.
+        print(f'Describe Correlation : \n{corr_mat}')
+        heat_map(corr_mat)
+        # Reasons behind employees leaving are influenced by
+        # satisfaction_level -> salary -> work_accident -> department
+        ########### Correlation Matrix ############
+
+        ########## Distribution plot ##############
+        #Outcome of the Distributions:
+        # Employee Satisfaction (satisfaction_level)
+            # The distribution is bimodal, with peaks at low (around 0.1) and high (around 0.7-0.8) satisfaction levels.
+            # This suggests that employees tend to be either very satisfied or very dissatisfied.
+        hist_plot(data['satisfaction_level'])
+
+        # Employee Evaluation (last_evaluation)
+            # The distribution shows peaks around 0.5 and 0.85-1.0.
+            # This suggests two groups: moderate performers and highly evaluated employees.
+        hist_plot(data['last_evaluation'])
+
+        # Employee Average Monthly Hours (average_monthly_hours)
+            # The distribution is right-skewed, with peaks around 150-200 and 250+ hours.
+            # This suggests two work-hour groups: regular workers and overworked employees.
+        hist_plot(data['average_montly_hours'])
+        ########## Distribution plot ##############
+
+        # Filter the data for employees who left
+        df_left = data[data["left"] == 1]
+        columns_to_plot = ["satisfaction_level", "last_evaluation", "average_montly_hours"]
+        # Plot the distribution for employees who left
+        plt.figure(figsize=(15, 5))
+        for i, col in enumerate(columns_to_plot, 1):
+            plt.subplot(1, 3, i)
+            sb.histplot(df_left[col], kde=True, bins=30, color="red")
+            plt.title(f"Distribution of {col} (Employees who left)")
+        plt.tight_layout()
+        plt.show()
+
+        ########## 2.3 Bar plot ##############
+        # 2.3. Draw the bar plot of the employee project count of both employees
+        # who left and stayed in the organization (use column number_project
+        # and hue column left), and give your inferences from the plot.
+        ########## Inference from countPlot is Employees with low number of projects which is 2 and Employees with high number of projects(7) left the company #################
+        count_plot(data, 'number_project', 'left', 'Employee Project Count by Left/Stayed Status', 'Number of Projects',
+                   'Count of Employees', "count")
+        count_plot(data,'number_project','left','Employee Project Percent by Left/Stayed Status','Number of Projects',
+                   'Percentage of Employees','percent')
+        count_plot(data, 'number_project', 'left', 'Employee Project Proportion by Left/Stayed Status', 'Number of Projects',
+                   'Proportion of Employees', 'proportion')
 
         ######### Splitting Data ##########
         X = data.drop(columns='average_montly_hours')
