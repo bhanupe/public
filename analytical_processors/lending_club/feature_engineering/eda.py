f457f79396acacbc3cd661bd9a303a3cb58f0b83
Vijay Peddimsetti <vpeddimsetti@apple.com>
2025-04-27 12:47:46 -0500

    PG AIML - ML - Lending Club Loan Data Analysis

--- analytical_processors/lending_club/feature_engineering/eda.py
+++ analytical_processors/lending_club/feature_engineering/eda.py
@@ -1,8 +1,10 @@
 import pandas as pd
 # Set the display.max_columns option to None
 from analytical_processors.wrangling.insights import explain
+from analytical_processors.wrangling.prepare import encode
 
 data = pd.read_csv("../data/loan_data.csv")
 explain(data)
-data_not_delinq=data.filter(items=['delinq.2yrs'])
-explain(data_not_delinq)
+data_encoded = encode(data, int)
+explain(data_encoded)
+
--- analytical_processors/wrangling/prepare.py
+++ analytical_processors/wrangling/prepare.py
@@ -9,3 +9,9 @@ def convert_data_type(data, column, dest_type):
     if dest_type == 'datetime':
         data.Date = pd.to_datetime(column)
         return data
+
+
+def encode(data, datatype):
+    data_encoded = pd.get_dummies(data, dtype=datatype)
+    data_encoded.to_csv('../data/data_encoded.csv', index=False)
+    return data_encoded
